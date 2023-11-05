cd2e4c1dcb281539acadce2f330830e5ecb88a86
bdcoller <bdcoller@gmail.com>
2023-11-05 14:28:00 -0600

    Got 3d rotation and stretching working

--- SpringModel.cs
+++ SpringModel.cs
@@ -9,11 +9,15 @@ public partial class SpringModel : Node3D
 	CoilGen coil;
 	float L0;    // natural length of spring
 
-	Transform3D tr;
-	Vector3 ctrLoc;
-    Vector3 basisVecX;
+	Transform3D tr; // tranform object for the coil
+	Vector3 ctrLoc;   // location of coil center
+    Vector3 basisVecX; // basis vectors for coil orientation
     Vector3 basisVecY;
     Vector3 basisVecZ;
+
+	Vector3 dVec;     // used for calculating basis vectors
+	Vector3 pxRaw;
+	Vector3 px;
 
 	//------------------------------------------------------------------------
 	// _Ready: Called when the node enters the scene tree for the first time.
@@ -29,6 +33,8 @@ public partial class SpringModel : Node3D
         basisVecZ = new Vector3(0.0f, 0.0f, 1.0f);
         tr.Origin = ctrLoc;
         tr.Basis = Basis.Identity;
+
+		pxRaw = new Vector3(0.0f, 0.0f, 0.0f);
 	}
 
 	//------------------------------------------------------------------------
@@ -49,9 +55,29 @@ public partial class SpringModel : Node3D
 	//------------------------------------------------------------------------
 	public void PlaceEndPoints(Vector3 endA, Vector3 endB)
 	{
+		ctrLoc = 0.5f * (endA + endB);
 		
-		
+		dVec = endB-endA;
+		basisVecX = dVec.Normalized();
+		pxRaw.Y = basisVecX.Y;    pxRaw.Z = basisVecX.Z;
+		if(pxRaw.Length() < 0.001f){
+			basisVecZ.X = basisVecZ.Y = 0.0f;
+			basisVecZ.Z = 1.0f;
+		}
+		else{
+			px = pxRaw.Normalized();
+			basisVecZ = px.Cross(Vector3.Right);
+		}
+		 
+		basisVecY = basisVecZ.Cross(basisVecX);
 
+		tr.Origin = ctrLoc;
+		tr.Basis = new Basis(basisVecX, basisVecY, basisVecZ);
+
+		Transform = tr;
+
+		float sc = dVec.Length()/L0;
+		Scale = new Vector3(sc, 1.0f, 1.0f);
 	}
 
 
